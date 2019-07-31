12692cd47dd7e39b616d613696b187de05d741be
handsomewlb <15389183138@163.com>
2019-07-31 16:32:32 +0800

    spring-boot-logger-resource 日志和资源

---
+++ spring-boot-logger-resource/src/main/java/com/wlb/springbootloggerresource/SpringBootLoggerResourceApplication.java
@@ -0,0 +1,13 @@
+package com.wlb.springbootloggerresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggerResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggerResourceApplication.class, args);
+    }
+
+}
