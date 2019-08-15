27ca8f029d6849df5aff357442ada309c936d2ce
handsomewlb <15389183138@163.com>
2019-08-15 16:08:25 +0800

    spring-boot-ssm 整合ssm

---
+++ spring-boot-ssm/src/main/java/com/wlb/springbootssm/SpringBootSsmApplication.java
@@ -0,0 +1,13 @@
+package com.wlb.springbootssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSsmApplication.class, args);
+    }
+
+}
